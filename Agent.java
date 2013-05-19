ed4dd630d699b7d8098f87dfa62ef7c2d9ddafd5
Sam Bassett <sam.d.bassett@gmail.com>
2013-05-19 15:01:05 +1000

    Style changes, fixed a bug which didn't update map after dynamite used
    Next thing to do is some form of rudimentary pathfinding, I was just
    going to get the agent to walk around and discover the map on its own
    to begin with.

--- Agent.java
+++ Agent.java
@@ -2,10 +2,11 @@
 /*  Agent.java
 /*  Agent for Text-Based Adventure Game
  *  John Aiden Rohde (z3343276)
- *  Sam Basset (z  )
+ *  Sam Basset (z3372468)
 /*  COMP3411 Artificial Intelligence
 /*  UNSW Session 1, 2013
-*/
+ */
+
 
 import java.util.*;
 import java.io.*;
@@ -35,7 +36,7 @@ public class Agent {
    private int num_dynamites_held = 0;
 
 
-   public char get_action( ) {
+   public char get_action(char[][] view) {
 
       // REPLACE THIS CODE WITH AI TO CHOOSE ACTION
 
@@ -89,26 +90,27 @@ public class Agent {
       int new_row, new_col;
       char ch;
 
-      if( action == 'i') {
+      // run the first time only
+      if ( action == 'i') {
          init_world(view);
          return( true );
       }
 
-      if(( action == 'L' )||( action == 'l' )) {
+      // make changes to direction only, not map, if dir'n changed
+      if (( action == 'L' )||( action == 'l' )) {
          dirn = ( dirn + 1 ) % 4;
          return( true );
-      }
-      else if(( action == 'R' )||( action == 'r' )) {
+      } else if (( action == 'R' )||( action == 'r' )) {
          dirn = ( dirn + 3 ) % 4;
          return( true );
-      }
-      else {
+      } else { 
+         // if direction not changed, look ahead 1 space to see what's there
          d_row = 0; d_col = 0;
          switch( dirn ) {
-          case NORTH: d_row = -1; break;
-          case SOUTH: d_row =  1; break;
-          case EAST:  d_col =  1; break;
-          case WEST:  d_col = -1; break;
+         case NORTH: d_row = -1; break;
+         case SOUTH: d_row =  1; break;
+         case EAST:  d_col =  1; break;
+         case WEST:  d_col = -1; break;
          }
          new_row = row + d_row;
          new_col = col + d_col;
@@ -116,32 +118,39 @@ public class Agent {
          ch = map[new_row][new_col];
       }
 
+      // if no direction changes to be made:
       switch( action ) {
-         case 'F': case 'f': // forward
-            switch( ch ) {   // can't move into an obstacle
-            case '*': case 'T': case '-':
-               return( false );
-            }
-            map[row][col] = ' '; // clear current location
-            row = new_row;
-            col = new_col;
-            map[row][col] = ' '; // clear new location
+      case 'F': case 'f': // forward
+         switch( ch ) {   // can't move into an obstacle
+         case '*': case 'T': case '-':
+            return( false );
+         }
+         map[row][col] = ' '; // clear current location
+         row = new_row;
+         col = new_col;
+         map[row][col] = ' '; // clear new location
 
-            switch( ch ) {
-             case 'a': have_axe  = true;     break;
-             case 'k': have_key  = true;     break;
-             case 'g': have_gold = true;     break;
-             case 'd': num_dynamites_held++; break;
-             case '~': game_lost = true;     break;
-            }
-            if( have_gold &&( row == irow )&&( col == icol )) {
-               game_won = true;
-            }
-            update_map(view);
-            return( true );
-         case 'L': case 'R': case 'C': case 'O': case 'B':
-         case 'l': case 'r': case 'c': case 'o': case 'b':
-            return(true);
+         switch( ch ) {
+         case 'a': 
+            have_axe  = true;     break;
+         case 'k': 
+            have_key  = true;     break;
+         case 'g': 
+            have_gold = true;     break;
+         case 'd': 
+            num_dynamites_held++; break;
+         case '~': 
+            game_lost = true;     break;
+         }
+         if( have_gold &&( row == irow )&&( col == icol )) {
+            game_won = true;
+         }
+         update_map(view);
+         return( true );
+      case 'L': case 'R': case 'C': case 'O': case 'B':
+      case 'l': case 'r': case 'c': case 'o': case 'b':
+         update_map(view);
+         return(true);
       }
       return(false);
    }
@@ -169,10 +178,10 @@ public class Agent {
       for( i = -2; i <= 2; i++ ) {
          for( j = -2; j <= 2; j++ ) {
             switch( dirn ) { // Adjust the orientation
-             case NORTH: r = row+i; c = col+j; break;
-             case SOUTH: r = row-i; c = col-j; break;
-             case EAST:  r = row+j; c = col-i; break;
-             case WEST:  r = row-j; c = col+i; break;
+            case NORTH: r = row+i; c = col+j; break;
+            case SOUTH: r = row-i; c = col-j; break;
+            case EAST:  r = row+j; c = col-i; break;
+            case WEST:  r = row-j; c = col+i; break;
             }
             map[r][c] = view[2+i][2+j];
          }
@@ -189,14 +198,14 @@ public class Agent {
          for( c=0; c < map[r].length; c++ ) {
             if(( r == row )&&( c == col )) { // agent is here
                switch( dirn ) {
-                case NORTH: ch = '^'; break;
-                case EAST:  ch = '>'; break;
-                case SOUTH: ch = 'v'; break;
-                case WEST:  ch = '<'; break;
+               case NORTH: ch = '^'; break;
+               case EAST:  ch = '>'; break;
+               case SOUTH: ch = 'v'; break;
+               case WEST:  ch = '<'; break;
                }
             }
             else {
-              ch = map[r][c];
+               ch = map[r][c];
             }
             System.out.print( ch );
          }
@@ -206,11 +215,11 @@ public class Agent {
    }
 
    public static void main( String[] args ){
-      InputStream in  = null;
+      InputStream  in = null;
       OutputStream out= null;
       Socket socket   = null;
       Agent  agent    = new Agent();
-      char view[][] = new char[5][5];
+      char   view[][] = new char[5][5];
       //Special action i used to intialize the map
       char   action   = 'i';
       int port;
@@ -250,7 +259,7 @@ public class Agent {
             }
             agent.update_world( action, view ); //Update the map and other things
             agent.print_view( view ); // COMMENT THIS OUT BEFORE SUBMISSION
-            action = agent.get_action( );
+            action = agent.get_action(view);
             out.write( action );
          }
       }
