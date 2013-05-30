1d9e157a1d49d376029f3c5bc938f81a52217eb3
Sam Bassett <sam.d.bassett@gmail.com>
2013-05-30 10:13:22 +1000

    Resolved conflict

--- Point.java
+++ Point.java
@@ -5,21 +5,12 @@ public class Point {
 	final static int WEST   = 2;
 	final static int SOUTH  = 3;
 
-<<<<<<< HEAD
-<<<<<<< HEAD
+
    // A link to another Point for use in the Astar pathfinding function
 	public Point parent;
-=======
-=======
->>>>>>> 81dc39f76f4524c44f3fd8f90ff4f715ba3628fb
-   // Variables for use in the Astar
-   public Point parent;
-   public int f;
-   public int g;
-<<<<<<< HEAD
->>>>>>> 81dc39f76f4524c44f3fd8f90ff4f715ba3628fb
-=======
->>>>>>> 81dc39f76f4524c44f3fd8f90ff4f715ba3628fb
+	
+	public int f;
+	public int g;
 
 	public int row;
 	public int col;
@@ -48,24 +39,17 @@ public class Point {
 		dirn = NORTH;
 	}
 
-<<<<<<< HEAD
-<<<<<<< HEAD
+
 	public void setParent (Point parent){
 		this.parent = parent;
 	}
-=======
-=======
->>>>>>> 81dc39f76f4524c44f3fd8f90ff4f715ba3628fb
-   public void setF (int f){
-      this.f = f;
-   }
 
-   public void setG (int g){
-      this.g = g;
-   }
+	public void setF (int f){
+		this.f = f;
+	}
 
-   public void setParent (Point parent){
-      this.parent = parent;
-   }
->>>>>>> 81dc39f76f4524c44f3fd8f90ff4f715ba3628fb
+	public void setG (int g){
+		this.g = g;
+	}
+
 }
