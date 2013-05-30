8cecb7f66ed0ead5d8b6d6a31a9e781f74249623
John Rohde <johnaidenrohde@gmail.com>
2013-05-30 10:14:20 +1000

    Fixed

--- Point.java
+++ Point.java
@@ -5,21 +5,12 @@ public class Point {
 	final static int WEST   = 2;
 	final static int SOUTH  = 3;
 
-<<<<<<< HEAD
-<<<<<<< HEAD
    // A link to another Point for use in the Astar pathfinding function
 	public Point parent;
-=======
-=======
->>>>>>> 81dc39f76f4524c44f3fd8f90ff4f715ba3628fb
    // Variables for use in the Astar
    public Point parent;
    public int f;
    public int g;
-<<<<<<< HEAD
->>>>>>> 81dc39f76f4524c44f3fd8f90ff4f715ba3628fb
-=======
->>>>>>> 81dc39f76f4524c44f3fd8f90ff4f715ba3628fb
 
 	public int row;
 	public int col;
@@ -48,14 +39,9 @@ public class Point {
 		dirn = NORTH;
 	}
 
-<<<<<<< HEAD
-<<<<<<< HEAD
 	public void setParent (Point parent){
 		this.parent = parent;
 	}
-=======
-=======
->>>>>>> 81dc39f76f4524c44f3fd8f90ff4f715ba3628fb
    public void setF (int f){
       this.f = f;
    }
@@ -64,8 +50,4 @@ public class Point {
       this.g = g;
    }
 
-   public void setParent (Point parent){
-      this.parent = parent;
-   }
->>>>>>> 81dc39f76f4524c44f3fd8f90ff4f715ba3628fb
 }
